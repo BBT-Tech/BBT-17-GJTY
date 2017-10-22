cef3b83d74d814d12eb47c902edbe872931962f6
Kingsley <kingsley.xym@gmail.com>
2017-10-22 23:32:16 +0800

    Initial Status Page

--- assets/js/reserve.js
+++ assets/js/reserve.js
@@ -1,38 +1,44 @@
-var status = new Vue({
-	el: "#status",
-	data: {
-		num: 17,
-		cur_num: 13,
-		pre_num: 3,
-		time: 15
-	}
-})
-
-$("#reserve").submit(function(e) {
-	e.preventDefault();
-
-	data = {};
-	$.each(
-		$(this).serializeArray(),
-		function(i, v) { data[v.name] = v.value; }
-	);
-
-	$.post(
-		'./user/register/',
-		JSON.stringify(data),
-		function(rsp) {
-			if (rsp.status == 0) {
-				status.num = rsp.data.userPos;
-				status.cur_num = rsp.data.curPos;
-				status.pre_num = rsp.data.queueLength - curPos - 1;
-				status.time = status.pre_num * rsp.data.avgServeTime;
-			} else {
-				alert(rsp.data.errorMessage);
-			}
+if (true/*document.referrer == 'source'*/) {
+	$("#reserve").hide();
+	
+	var status = new Vue({
+		el: "#status",
+		data: {
+			num: 17,
+			cur_num: 13,
+			pre_num: 3,
+			time: 15
 		}
-	)
+	})
 
-	.fail(function() {
-		alert('操作出错，请联系管理员');
+	$("#reserve").submit(function(e) {
+		e.preventDefault();
+
+		data = {};
+		$.each(
+			$(this).serializeArray(),
+			function(i, v) { data[v.name] = v.value; }
+		);
+
+		$.post(
+			'./user/register/',
+			JSON.stringify(data),
+			function(rsp) {
+				if (rsp.status == 0) {
+					status.num = rsp.data.userPos;
+					status.cur_num = rsp.data.curPos;
+					status.pre_num = rsp.data.queueLength - curPos - 1;
+					status.time = status.pre_num * rsp.data.avgServeTime;
+				} else {
+					alert(rsp.data.errorMessage);
+				}
+			}
+		)
+
+		.fail(function() {
+			alert('操作出错，请联系管理员');
+		});
 	});
-});
+} else {
+	window.location.href = './user/subscribeMsg/';
+}
