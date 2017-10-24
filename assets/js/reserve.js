375b915f90995c613da5a261974244d20f805ad0
Kingsley <kingsley.xym@gmail.com>
2017-10-25 01:14:20 +0800

    :bug: Fix A Possible Waiting Number Related Bug

--- assets/js/reserve.js
+++ assets/js/reserve.js
@@ -1,5 +1,5 @@
 // var formSource = 'https://100steps.withcic.cn/2017_gjty/user/onSubscribed/';
-var formSource = '';
+var formSource = 's';
 var phoneReg = /^1[0-9]{10}$/;
 var emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -57,7 +57,10 @@ if (document.referrer.indexOf(formSource) == 0) {
 						$("#position").show(1300);
 					});
 				} else {
-					alert(response.errorMessage);
+					if (response.status == -1)
+						window.location.href = response.redirect;
+					else
+						alert(response.errorMessage);
 				}
 			}
 		).fail(function() {
@@ -70,13 +73,21 @@ if (document.referrer.indexOf(formSource) == 0) {
 		'./test_isUserInQueue.php',
 		function(response) {
 			if (response.status == 0) {
-				if (response.data.isInQueue) {
+				var d = response.data;
+				if (d.isInQueue) {
+					var waiting = d.userPos - d.curPos;
+					if (waiting <= -5) {
+						alert('您已过号五个以上，请重新取号> <');
+						window.location.href = './guide.html';
+					}
+					waiting = waiting > 0 ? waiting : 0;
+
 					$("#success").hide();
 					$("#status-title").show();
 
-					$("#position").text(response.data.userPos);
-					$("#waiting").text(response.data.userPos - response.data.curPos);
-					$("#time").text($("#waiting").text() * response.data.avgServeTime);
+					$("#position").text(d.userPos);
+					$("#waiting").text(waiting);
+					$("#time").text(waiting * d.avgServeTime);
 
 					$("#position").hide();
 					$("#status").fadeIn(1200, function() {
@@ -86,35 +97,38 @@ if (document.referrer.indexOf(formSource) == 0) {
 					window.location.href = './guide.html';
 				}
 			} else {
-				alert(response.errorMessage);
+				if (response.status == -1)
+					window.location.href = response.redirect;
+				else
+					alert(response.errorMessage);
 			}
 		}
 	).fail(function() {
 		alert('获取状态信息失败，请联系管理员');
 	});
-
-	$("#fresh").click(function () {
-		$.getJSON(
-			// './queueinfo.json',
-			'./test_queueinfo.php',
-			function(d) {
-				var waiting = $("#position").text() - d.curPos;
-				waiting = waiting > 0 ? waiting : 0;
-				if (waiting <= -5) {
-					alert('您已过号五个，请重新取号><');
-					window.location.href = './guide.html';
-				}
-
-				$("#waiting").fadeOut(300, function() {
-					$("#waiting").text(waiting).fadeIn(500);
-				});
-
-				$("#time").fadeOut(300, function() {
-					$("#time").text(waiting * d.avgServeTime).fadeIn(500);
-				});
-			}
-		).fail(function() {
-			alert('获取队列信息失败，请联系管理员');
-		});
-	});
 }
+
+$("#fresh").click(function () {
+	$.getJSON(
+		// './queueinfo.json',
+		'./test_queueinfo.php',
+		function(d) {
+			var waiting = $("#position").text() - d.curPos;
+			if (waiting <= -5) {
+				alert('您已过号五个以上，请重新取号> <');
+				window.location.href = './guide.html';
+			}
+			waiting = waiting > 0 ? waiting : 0;
+
+			$("#waiting").fadeOut(300, function() {
+				$(this).text(waiting).fadeIn(500);
+			});
+
+			$("#time").fadeOut(300, function() {
+				$(this).text(waiting * d.avgServeTime).fadeIn(500);
+			});
+		}
+	).fail(function() {
+		alert('获取队列信息失败，请联系管理员');
+	});
+});
