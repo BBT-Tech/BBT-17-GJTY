1e8398d8aa30da40edff47637b9df88e44fdb3fe
Kingsley <kingsley.xym@gmail.com>
2017-10-26 12:24:17 +0800

    Add Reservable Status Handler

--- assets/js/reserve.js
+++ assets/js/reserve.js
@@ -1,4 +1,5 @@
-// var formSource = 'https://100steps.withcic.cn/2017_gjty/user/onSubscribed/';
+// var formSource = 'https://100steps.withcic.cn/2017_gjty/user/onSubscribedMsg/';
+
 var formSource = 's';
 var phoneReg = /^1[0-9]{10}$/;
 var emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
