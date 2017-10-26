441947387030c60970779383f4b922c5e2b38f4a
Kingsley <kingsley.xym@gmail.com>
2017-10-26 15:31:04 +0800

    :tada: Finish Guide Module

---
+++ assets/js/guide.js
@@ -0,0 +1,57 @@
+function setVerticalAlign(e) {
+	divBase = e == "#closed" ? 4 : 2;
+
+	$(e).show();
+	$("body").css("margin-top", function() {
+		return (this.scrollHeight - $("#guide").prop('scrollHeight')) / divBase;
+	});
+}
+
+$.get(
+	// './user/isUserInQueue/',
+	'./test_isUserInQueue.php',
+	function(re) {
+		if (re.status == 0 || re.status == -1) {
+			regStatus = (re.status == -1 ? 0 : re.data.isRegisterAble);
+
+			if (!re.data.isInQueue) {
+				switch(regStatus) {
+					case 0:
+						setVerticalAlign("#welcome");
+						$("#auth").click(function() {
+							location.href = './user/subscribeMsg/';
+						});
+						break;
+
+					case 1:
+						setVerticalAlign("#pending");
+						$("#fresh").click(function() {
+							$.get(
+								// './user/isUserInQueue/',
+								'./test_isUserInQueue.php',
+								function(r) {
+									if (r.data.isRegisterAble == 0)
+										location.href = './guide.html';
+									else
+										$("#pending-info").fadeOut(300, function() {
+											$(this).fadeIn(500);
+										});
+								}
+							)
+						});
+						break;
+
+					case -1:
+						setVerticalAlign("#closed");
+						break;
+				}
+			} else {
+				location.href = './index.html';
+			}
+		} else {
+			alert(re.errorMessage);
+		}
+	}
+).fail(function() {
+	alert('获取状态信息失败，请联系管理员');
+});
--- test_isUserInQueue.php
+++ test_isUserInQueue.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json');
 
-// echo '{"status": 0, "data":{"isInQueue": false}}';
+// echo '{"status": -1, "data":{"isInQueue": false, "isRegisterAble": 0}}';
+// echo '{"status": 0, "data":{"isInQueue": false, "isRegisterAble": 0}}';
+// echo '{"status": 0, "data":{"isInQueue": false, "isRegisterAble": 1}}';
+// echo '{"status": 0, "data":{"isInQueue": false, "isRegisterAble": -1}}';
 echo '{"status": 0, "data":{"isInQueue": true, "userPos": 17, "curPos": 13, "queueLength":233, "avgServeTime":5 }}';
