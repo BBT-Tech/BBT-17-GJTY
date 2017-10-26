05ebad1a7a5c2749bab4921f01836197138c5535
Kingsley <kingsley.xym@gmail.com>
2017-10-27 00:46:54 +0800

    Optimize Code Of Guide Page

--- assets/js/guide.js
+++ assets/js/guide.js
@@ -1,3 +1,50 @@
+$.get(
+	// './user/isUserInQueue/',
+	'./test_isUserInQueue.php',
+	function(r) {
+		if (r.status == 0 || r.status == -1) {
+			switch(r.data.isRegisterAble) {
+				case 0:
+					if ((typeof r.data.isInQueue != 'undefined')
+						&& (r.data.curPos - r.data.userPos >= 5))
+						location.href = './index.html';
+
+					setVerticalAlign("#welcome");
+					$("#auth").click(function() {
+						location.href = './user/subscribeMsg/';
+					});
+					break;
+
+				case 1:
+					setVerticalAlign("#pending");
+					$("#fresh").click(function() {
+						$.get(
+							// './user/isUserInQueue/',
+							'./test_isUserInQueue.php',
+							function(re) {
+								if (re.data.isRegisterAble == 0)
+									location.reload();
+								else
+									$("#pending-info").fadeOut(300, function() {
+										$(this).fadeIn(500);
+									});
+							}
+						)
+					});
+					break;
+
+				case -1:
+					setVerticalAlign("#closed");
+					break;
+			}
+		} else {
+			alert(r.errorMessage);
+		}
+	}
+).fail(function() {
+	alert('获取状态信息失败，请联系管理员');
+});
+
 function setVerticalAlign(e) {
 	divBase = e == "#closed" ? 3 : 2;
 
@@ -6,52 +53,3 @@ function setVerticalAlign(e) {
 		return (this.scrollHeight - $("#guide").prop('scrollHeight')) / divBase;
 	});
 }
-
-$.get(
-	// './user/isUserInQueue/',
-	'./test_isUserInQueue.php',
-	function(re) {
-		if (re.status == 0 || re.status == -1) {
-			regStatus = (re.status == -1 ? 0 : re.data.isRegisterAble);
-
-			if (!re.data.isInQueue) {
-				switch(regStatus) {
-					case 0:
-						setVerticalAlign("#welcome");
-						$("#auth").click(function() {
-							location.href = './user/subscribeMsg/';
-						});
-						break;
-
-					case 1:
-						setVerticalAlign("#pending");
-						$("#fresh").click(function() {
-							$.get(
-								// './user/isUserInQueue/',
-								'./test_isUserInQueue.php',
-								function(r) {
-									if (r.data.isRegisterAble == 0)
-										location.href = './guide.html';
-									else
-										$("#pending-info").fadeOut(300, function() {
-											$(this).fadeIn(500);
-										});
-								}
-							)
-						});
-						break;
-
-					case -1:
-						setVerticalAlign("#closed");
-						break;
-				}
-			} else {
-				location.href = './index.html';
-			}
-		} else {
-			alert(re.errorMessage);
-		}
-	}
-).fail(function() {
-	alert('获取状态信息失败，请联系管理员');
-});
